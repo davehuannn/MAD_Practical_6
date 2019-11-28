4fe107f4b7fc81b4e95033e3f1eb5cf39d38ce4e
Dave Huan <davehuan123@gmail.com>
2019-11-28 15:14:47 +0800

    Initial Commit

---
+++ Dave Practical 6/ViewController.swift
@@ -0,0 +1,20 @@
+//
+//  ViewController.swift
+//  Dave Practical 6
+//
+//  Created by MAD2_P03 on 28/11/19.
+//  Copyright © 2019 Dave Huan. All rights reserved.
+//
+
+import UIKit
+
+class ViewController: UIViewController {
+
+    override func viewDidLoad() {
+        super.viewDidLoad()
+        // Do any additional setup after loading the view.
+    }
+
+
+}
+
